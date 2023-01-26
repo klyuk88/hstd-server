6f2d96a5fc515bd7c8b7c884bb9b95f4fd5d14c8
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-01-26 19:33:36 +0300

    fix register note

--- src/api/manuscript/content-types/manuscript/lifecycles.js
+++ src/api/manuscript/content-types/manuscript/lifecycles.js
@@ -6,6 +6,12 @@ module.exports = {
 
         const locale = result.locale
 
+        const subject = {
+            ru: 'Благодарим Вас за представление доклада на конференцию HSTD 23',
+            en: 'Thank you for submitting your paper to the conference HSTD 23'
+        }
+
+
         const message = `
         <p style="font-size: 16px">
         Dear Participant!<br>
@@ -22,7 +28,7 @@ module.exports = {
         Благодарим Вас за представление доклада на конференцию! Процесс рассмотрения займет не более 2 недель, после чего вы получите письмо с решением Программного комитета.
         </p>
         <p style="font-size: 16px">
-        Искренне,<br>
+        С Уважением,<br>
         Оргкомитет HSTD 2023
         </p>`
 
@@ -32,7 +38,7 @@ module.exports = {
             const author = result.authors[index];
 
             if (author.corresponding) {
-                strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? messageRu : message, 'Благодарим Вас за представление доклада на конференцию HSTD 23')
+                strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? messageRu : message, locale === 'ru' ? subject.ru : subject.en)
             }
 
         }
@@ -42,26 +48,21 @@ module.exports = {
         const {
             result
         } = event;
+        const locale = result.locale
 
         const entry = await strapi.entityService.findOne('api::manuscript.manuscript', result.id, {
             populate: {
                 authors: true,
                 statusWork: true
             },
+            locale: locale
         });
+        if (entry.authors.length > 0 && entry.statusWork) {
+            const subject = {
+                ru: 'Решение по вашей статье на HSTD 2023',
+                en: 'Decision on your paper at HSTD 2023'
+            }
 
-        if (entry.authors.length > 0) {
-            
-            const locale = result.locale
-            const message = `
-            <p style="font-size: 16px">
-            Dear Participant!<br>
-            Thank you for submitting your paper to the conference! The review process will take no more than 2 weeks, after that you will receive a letter with decision of the Program Committee.
-            </p>
-            <p style="font-size: 16px">
-            Sincerely,<br>
-            HSTD 2023 Organizing Committee
-            </p>`
 
             const reviseMessage = {
                 ru: `
@@ -70,7 +71,7 @@ module.exports = {
                 По решению Программного комитета ваша статья нуждается в доработке. Вы можете прочитать комментарии рецензентов в личном кабинете в поле Решение. Ждем вашу статью после доработки.
                 </p>
                 <p style="font-size: 16px">
-                Искренне,<br>
+                С Уважением,<br>
                 Оргкомитет HSTD 2023
                 </p>`,
                 en: `
@@ -91,7 +92,7 @@ module.exports = {
                 С сожалением сообщаем Вам, что в соответствии с решением Программного комитета Ваш доклад не может быть принят к участию в конференции. Вы можете прочитать комментарии рецензента в личном кабинете в поле Решение.
                 </p>
                 <p style="font-size: 16px">
-                Искренне,<br>
+                С Уважением,<br>
                 Оргкомитет HSTD 2023
                 </p>`,
                 en: `
@@ -111,7 +112,7 @@ module.exports = {
                 Мы рады сообщить вам, что ваша статья принята к участию в конференции. В ближайшее время вы сможете оплатить регистрационный взнос в личном кабинете. Подробную программу вы получите после 10 августа. Если у вас есть какие-либо вопросы, не стесняйтесь обращаться к оргкомитету конференции.
                 </p>
                 <p style="font-size: 16px">
-                Искренне,<br>
+                С Уважением,<br>
                 Оргкомитет HSTD 2023
                 </p>`,
                 en: `
@@ -129,24 +130,22 @@ module.exports = {
             for (let index = 0; index < entry.authors.length; index++) {
                 const author = entry.authors[index];
 
-                if (author.corresponding && result.statusWork.slug === 'revise') {
-                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? reviseMessage.ru : reviseMessage.en)
+                if (author.corresponding && entry.statusWork.slug === 'revise') {
+                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? reviseMessage.ru : reviseMessage.en, locale === 'ru' ? subject.ru : subject.en)
 
-                } else if (author.corresponding && result.statusWork.slug === 'reject') {
-                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? rejectMessage.ru : rejectMessage.en)
-                } else if (author.corresponding && result.statusWork.slug === 'accept') {
+                } else if (author.corresponding && entry.statusWork.slug === 'reject') {
+                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? rejectMessage.ru : rejectMessage.en, locale === 'ru' ? subject.ru : subject.en)
 
-                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? acceptMessage.ru : acceptMessage.en)
+                } else if (author.corresponding && entry.statusWork.slug === 'accept') {
+
+                    strapi.service('api::manuscript.manuscript').sendMail(author.email, locale === 'ru' ? acceptMessage.ru : acceptMessage.en, locale === 'ru' ? subject.ru : subject.en)
                 }
 
             }
 
-
         } else {
             return
         }
-
-
 
 
     }
--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,5 +1,6 @@
 module.exports = (plugin) => {
     plugin.controllers.user.update = async (ctx) => {
+        console.log(ctx.request);
         try {
             const entry = await strapi.entityService.update(
                 'plugin::users-permissions.user',
@@ -7,7 +8,7 @@ module.exports = (plugin) => {
                     data: ctx.request.body
                 }
             );
-            strapi.service('api::manuscript.manuscript').sendMail(ctx.state.user.email, `Thank you for registration as listener.`, `Registration as listener at HSTD 2023`)
+            // strapi.service('api::manuscript.manuscript').sendMail(ctx.state.user.email, `Thank you for registration as listener.`, `Registration as listener at HSTD 2023`)
             ctx.body = entry;
         } catch (error) {
             ctx.body = error
